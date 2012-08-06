584f2b1f5ea7d5f3daa4d2bf1bbba13f591515fd
Ashok Menon <amenon94@gmail.com>
2012-08-06 16:24:49 +0100

    Fix to accomodate Safari

--- public/lib/displayLink.js
+++ public/lib/displayLink.js
@@ -1,5 +1,12 @@
 (function() {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    var requestAnimationFrame =       window.requestAnimationFrame 
+                           ||    window.mozRequestAnimationFrame 
+                           || window.webkitRequestAnimationFrame 
+                           ||     window.msRequestAnimationFrame
+                           || function( callback, element ) {
+
+        window.setTimeout( callback, 1000 / 60 );
+
+   };
   window.requestAnimationFrame = requestAnimationFrame;
 })();
