f15c50a9b0f86c5f9d0923fb6bf47aaa702db311
Ashok Menon <amenon94@gmail.com>
2012-08-07 17:33:39 +0100

    Cleaned up vertices

---
+++ public/js/twitterModel.js
@@ -0,0 +1,17 @@
+function TwitterModel() {
+    
+    this.graph = new Array();
+
+}
+
+TwitterModel.prototype.getData = function( id ) {
+    
+};
+
+TwitterModel.prototype.getFollowers = function( id ) {
+
+};
+
+TwitterModel.prototype.getFriends = function( id ) {
+    // body...
+};
