84fcc210e37fda8336294dbc78c1486672b3a2f0
mpickering <matthewtpickering@gmail.com>
2012-08-15 01:42:06 +0100

    Started added hooks for the progress bar into the javascript

--- public/js/buttonEvents.js
+++ public/js/buttonEvents.js
@@ -2,5 +2,11 @@ var searchBtnHandler = function () {
 
     var inputID = $( "#searchbox" ).val();
     model.buildGraphFor(        inputID );
+    $(  '#progressContainer').slideDown();
 
 };
+
+function updateProgressBar(percent){
+	var width = 		 $('#progressContainer').width();
+	$('#progressBar').animate({"width": width*percent });
+}
